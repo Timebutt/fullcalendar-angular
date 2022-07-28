22e0b33e8a5ca30c35b5af56581a4466a7f55691
Adam Shaw <arshaw@users.noreply.github.com>
2022-07-28 15:14:41 -0400

    no more angular-ivy. is defacto now

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { FullCalendarModule } from '@fullcalendar/angular-ivy';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FullCalendarModule } from '@fullcalendar/angular-ivy';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 @NgModule({
   declarations: [
